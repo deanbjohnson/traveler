d2f163b3431faa9812945aaae01411ecb88d305b
Dean Berlin Johnson <deanberlinjohnson@gmail.com>
2025-08-23 23:05:48 -0700

    Add webpack config to handle lightningcss issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ['@tailwindcss/postcss'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
